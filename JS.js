730e8975e8f5086c23f215f7e6cd47cf88d43fcf
miksplease <micovelado518@gmail.com>
2024-05-29 22:15:31 +0800

    initial commit

---
+++ JS.js
@@ -0,0 +1,14 @@
+document.getElementById('hob').addEventListener('click', function() {
+    const sec1 = document.querySelector('.sec1');
+    sec1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('ld').addEventListener('click', function() {
+    const sec2 = document.querySelector('.sec2');
+    sec2.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('socmed').addEventListener('click', function() {
+    const sec3 = document.querySelector('.sec3');
+    sec3.scrollIntoView({ behavior: 'smooth' });
+});
